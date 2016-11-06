7a03efda269f9daf7ed14c0a4b30162eb88e54e3
Jonathon Carstens <jon.carstens@gmail.com>
2016-11-06 12:04:21 -0800

    Merge branch 'master' into splitting
    # Conflicts:
    #	discrete_model.m

--- discrete_model.m
+++ discrete_model.m
@@ -12,7 +12,7 @@ prnt = input(prompt,'s');
 
 %% Node Control from .csv
 heatZone = csvread('SnakeInput.csv');
-heatZoneSize = size(heataZone);
+heatZoneSize = size(heatZone);
 heatZoneV = reshape(heatZone,numel(heatZone),1);
 inputNodes = sum(heatZoneV);
 
@@ -230,82 +230,12 @@ end
 tempFinal = y(end,:);
 maxTemp = max(tempFinal);
 minTemp = min(tempFinal);
-% tenCont = floor((maxTemp-minTemp)/10);
-tenCont = 10;
-tempInitStr = [',it' num2str(tempInit)];
-powerInputStr = [',pi' num2str(Qs)];
+
+tenCont = floor((maxTemp-minTemp)/10);
 
 M1 = M(:,:,2);
 
-% figure
-% 
-% g = 4;
-% 
-% for j = 1:g
-%     subplot(g/2,2,j)
-%     contourf(M(:,:,j*floor(length(t)/g)),'ShowText','on','LevelStepMode',...
-%         'manual','LevelStep',tenCont);
-%     xlabel('x position (node)'); ylabel('y position (node)')
-%     lbl = num2str(t(j*floor(length(t)/g)));
-%     lbl = [lbl,' seconds'];
-%     ttl = ['Time = ',lbl]; title(ttl);
-%     set(gca,'Ydir','reverse');
-%     axis equal
-% end
-% pathStr = '/Users/jonathon/Documents/Thesis/GitRepo/Thermal-Model/latex/figures/';
-
-% nameStr = 'Transient';
-% pathStr = strcat(pathStr,simTitle,nameStr,tempInitStr,powerInputStr);
-% if strcmp(prnt,'Y') == 1
-%     print(pathStr,'-depsc')
-% else
-% end
-
-bRight = ['Bottom Right (' num2str(n) ',' num2str(n) ')'];
-
-figure
-plot(t,y(:,1),'LineWidth',2)
-hold on
-plot(t,y(:,n),'-.','LineWidth',2)
-plot(t,y(:,m*n),'LineWidth',2)
-plot(t,y(:,m*n-(n-1)),'--','LineWidth',2)
-plot(t,y(:,ceil(m*n/2)),'LineWidth',2)
-xlabel('Time (seconds)'); ylabel('Temperature (Degrees Celcius)');
-legend('Top Left (1,1)','Bottom Left',bRight,'Top Right','Center')
-title('Temperature Response')
-pathStr = '/Users/jonathon/Documents/Thesis/GitRepo/Thermal-Model/latex/figures/';
-nameStr = 'Response';
-pathStr = strcat(pathStr,simTitle,nameStr,tempInitStr,powerInputStr);
-if strcmp(prnt,'Y') == 1
-    print(pathStr,'-depsc')
-else
-end
-
-timeSim/60;
-timeSimSec = num2str(timeSim,3);
-timeSimMin = num2str(timeSim/60,3);
-if timeSim <= 60
-    disp(['Simulation Run time = ',timeSimSec,' seconds'])
-else
-    disp(['Simulation Run time = ',timeSimMin,' minutes'])
-end
-
-figure
-powerMap = [heatZone zeros(n,1)]; powerMap = [powerMap;zeros(1,n+1)];
-pcolor(powerMap);
-colormap(gray(2));
-colormap(flipud(colormap));
-xlabel('x position (node)'); ylabel('y position (node)')
-set(gca,'Ydir','reverse'); set(gca,'YTick',[]); set(gca,'XTick',[]);
-axis square
-title('Power Input Map')
-pathStr = '/Users/jonathon/Documents/Thesis/GitRepo/Thermal-Model/latex/figures/';
-nameStr = 'Map';
-pathStr = strcat(pathStr,simTitle,nameStr,tempInitStr,powerInputStr);
-if strcmp(prnt,'Y') == 1
-    print(pathStr,'-depsc')
-else
-end
+plots_script
 
 figure
 contourf(M(:,:,length(t)-1),10,'ShowText','on');
